e1dd69e563d72d3be928d2ced1837c730918312b
rayslan <60047996+rayslan@users.noreply.github.com>
2021-07-19 18:20:58 -0500

    Update key_pair.tf

--- key_pair.tf
+++ key_pair.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "acar" {
   key_name = "practise"
-  public_key = file("/home/admin/.ssh/id_rsa.pub")
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCcOKOBteOlNZw61/ZUCEwUqG6kgLjuMl13EIWyIOyb3ueJa8zf9ge6BwHmn/2tUG9lH5WfRX3OmzfKxSfi87tZ9ODjvNu4SOmypQK0YRU+O0OG0VPVovnR1FDFARAtD8XWp4Nrwu63mMsRGnISbrANWBK4mYM9YUT2N7WaeOxn6P17uxfVxsl+SckuQgmGq7p+hHpbkUtwnmHFlN5iEG4OP4V1A5UfHSyC04JQnd4uiwCLPrLJ15mICm51Aq7kEMz43tf1KFqU877foongPzO1JawPpnKU+swXSt9ZNXSs8oWNlzTchOBwQ/z/RHrb7GXsLb1S4hosRKrGJ/gEZaDv admin@ip-172-31-30-7"
   
 }
