58dc482a58a06770c228589c79523f531554d0f9
rayslan <60047996+rayslan@users.noreply.github.com>
2021-08-19 13:29:54 -0500

    Update key_pair.tf

--- key_pair.tf
+++ key_pair.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "acar" {
-  key_name = "practise"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
+# resource "aws_key_pair" "acar" {
+#   key_name = "practise"
+#   public_key = file("~/.ssh/id_rsa.pub")
+# }
