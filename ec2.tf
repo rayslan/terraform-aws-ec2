88a776241a341cce5b7e390d65cb515abb78808f
rayslan <60047996+rayslan@users.noreply.github.com>
2021-08-19 13:30:26 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami = data.aws_ami.ami.id 
   instance_type = "t2.micro"
   security_groups = [  aws_security_group.sec.name]
-  key_name = aws_key_pair.acar.key_name 
+  key_name = "tmstore"
 
   tags = {
     Name  = "Practise"
